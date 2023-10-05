ad5b47db8fd69f246a40db678fe14360ec451028
Igor Tonkopryadchenko <gtonky@gmail.com>
2023-10-05 16:03:22 +0200

    added state hashmap and 'stats' endpoint

--- web/src/main.rs
+++ web/src/main.rs
@@ -5,6 +5,7 @@ use common::MetricV1;
 use axum::{
     body::Bytes,
     debug_handler,
+    extract::State,
     Json,
     response::{Response, IntoResponse},
     routing::{get,post},
@@ -12,6 +13,7 @@ use axum::{
 };
 use serde_json::{Value, json};
 use serde::{Serialize,Deserialize};
+use std::{collections::HashMap, sync::{Arc, RwLock}};
 use tower_http::{
     services::{ServeDir, ServeFile},
     trace::TraceLayer,
@@ -20,21 +22,31 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use chrono::DateTime;
 use base64::{engine::general_purpose, Engine as _};
 
+type SharedState = Arc<RwLock<AppState>>;
+
+#[derive(Default)]
+struct AppState {
+    db: HashMap<String, u32>,
+}
 
 #[tokio::main]
 async fn main() {
-        tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "hello-wasm=debug,tower_http=debug,axum=debug".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+    tracing_subscriber::registry()
+    .with(
+        tracing_subscriber::EnvFilter::try_from_default_env()
+            .unwrap_or_else(|_| "hello-wasm=debug,tower_http=debug,axum=debug".into()),
+    )
+    .with(tracing_subscriber::fmt::layer())
+    .init();
+
+    let shared_state = SharedState::default();
 
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
+        .route("/stats", get(stats))
         .route("/v1/ingest", post(ingest_metrics_v1))
+        .with_state(Arc::clone(&shared_state))
         .merge(serve_static());
 
     // run it with hyper on localhost:3000
@@ -48,12 +60,30 @@ async fn main() {
 }
 
 #[debug_handler]
-async fn ingest_metrics_v1(body: Bytes) -> Json<Value> {
+async fn ingest_metrics_v1(State(state): State<SharedState>, body: Bytes) -> Json<Value> {
     println!("ingest_metrics_v1: {:?}", &body);
     let dec = general_purpose::STANDARD.decode(&body).unwrap();
     let dr: hello_wasm::v1::MetricV1 = rmp_serde::from_slice(&dec).unwrap();
 
+    let counter = match state.read().unwrap().db.get(&dr.auth_token) {
+        Some(count) => count.clone(),
+        None => 0
+    };
+
+    println!("{} counter: {}", &dr.auth_token, &counter);
+
+    state.write().unwrap().db.insert(dr.auth_token.clone(), counter + 1);
+
     Json(json!(dr))
+}
+
+#[debug_handler]
+async fn stats(State(state): State<SharedState>) -> Json<Value> {
+    let db = state.read().unwrap().db.clone();
+
+    println!("hashmap; {:?}", &db);
+
+    Json(json!(db))
 }
 
 fn serve_static() -> Router {
